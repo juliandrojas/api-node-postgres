565771a22c18191e4da550df8e72c010c60e1633
JULIAN ROJAS <juliandra140201@gmail.com>
2023-09-02 22:49:47 -0500

    Dashboard admin

--- src/controllers/index.controllers.js
+++ src/controllers/index.controllers.js
@@ -3,6 +3,20 @@ export const home = (req, res) => {
 }
 export const login = (req, res) => {
     res.render('login.ejs');
+}
+export const checkUser = (req, res) => {
     const email = req.body.email;
+    const password = req.body.password;
     console.log(email);
+    console.log(password);
+    // Expresión regular para verificar si el correo es "administrador@nombre.com"
+    const adminEmailRegex = /^administrador@nombre\.com$/i;
+    if (adminEmailRegex.test(email)) {
+        // Redirigir a la página de administrador
+        res.render('dashboardAdmin.ejs'); // Reemplaza 'pagina_administrador' con la ruta de tu página de administrador
+    } else {
+        // Redirigir a la página de empleados
+        res.render('pagina_empleados'); // Reemplaza 'pagina_empleados' con la ruta de tu página de empleados
+    }
+
 }
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,7 +1,8 @@
 import { Router } from 'express';
-import { home, login } from '../controllers/index.controllers.js';
+import { checkUser, home, login } from '../controllers/index.controllers.js';
 //Importamos los controladores
 const router = Router();
 router.get('/', home);
 router.get('/login', login);
+router.post('/checkUser', checkUser)
 export default router;
